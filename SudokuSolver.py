5ee719e630f3de188db7ae227fddd56b319bf37a
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-04-04 18:56:40 -0400

    Add columns functionality

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,7 +1,13 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def get_columns(self, rows):
+    for row in rows:
+        for i in range(1, 9):
+            columns[i].append(row[i])
+
 rows = []
+columns = [[], [], [], [], [], [], [], [], [], []]
 rows[1] = [0, 0, 0, 2, 6, 0, 7, 0, 1]
 rows[2] = [6, 8, 0, 0, 7, 0, 0, 9, 0]
 rows[3] = [1, 9, 0, 0, 0, 4, 5, 0, 0]
