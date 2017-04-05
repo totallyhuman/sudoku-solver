27901c66a5301c3fd93c34b6b9c72d7c59d14c36
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-04-05 16:39:57 -0400

    Format comments

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -4,23 +4,27 @@
 class Sudoku(object):
     """
     Sudoku(values) -> new instance
-    
+
     A class for a Sudoku puzzle.
-    
+
     Keyword arguments:
-    values -- an array containing all 81 values of a puzzle horizontally
-    
+    values   -- an array containing all 81 values of a puzzle horizontally
+
     Instance variables:
-    values -- an array containing all 81 values of a puzzle horizontally
-    rows -- a multi-dimensional array that stores 9 rows as arrays, each holding 9 values
-    columns -- a multi-dimensional array that stores 9 columns as arrays, each holding 9 values
-    squares -- a multi-dimensional array that stores 9 3x3 squares as arrays, each holding 9 values
+    values   -- an array containing all 81 values of a puzzle horizontally
+    rows     -- a multi-dimensional array that stores 9 rows as arrays, each
+                holding 9 values
+    columns  -- a multi-dimensional array that stores 9 columns as arrays, each
+                holding 9 values
+    squares  -- a multi-dimensional array that stores 9 3x3 squares as arrays,
+                each holding 9 values
     """
     values = []
     rows = [[], [], [], [], [], [], [], [], []]
     columns = [[], [], [], [], [], [], [], [], []]
     squares = [[], [], [], [], [], [], [], [], []]
-    
+    grid = {}
+
     def __init__(self, values):
         """See class docstring for details."""
         self.values = values
@@ -30,6 +34,7 @@ class Sudoku(object):
         self.parse_rows()
         self.parse_columns()
         self.parse_squares()
+        self.parse_grid()
 
     def parse_rows(self):
         """Parses the values into the rows array."""
@@ -62,15 +67,19 @@ values = [0, 0, 0, 2, 6, 0, 7, 0, 1,
           7, 0, 3, 0, 1, 8, 0, 0, 0]
 
 # Answer to example sudoku:
-# [4, 3, 5, 2, 6, 9, 7, 8, 1,
-#  6, 8, 2, 5, 7, 1, 4, 9, 3,
-#  1, 9, 7, 8, 3, 4, 5, 6, 2,
-#  8, 2, 6, 1, 9, 5, 3, 4, 7,
-#  3, 7, 4, 6, 8, 2, 9, 1, 5,
-#  9, 5, 1, 7, 4, 3, 6, 2, 8,
-#  5, 1, 9, 3, 2, 6, 8, 7, 4,
-#  2, 4, 8, 9, 5, 7, 1, 3, 6,
-#  7, 6, 3, 4, 1, 8, 2, 5, 9]
+# 4 3 5 | 2 6 9 | 7 8 1
+# 6 8 2 | 5 7 1 | 4 9 3
+# 1 9 7 | 8 3 4 | 5 6 2
+# ------+-------+------
+# 8 2 6 | 1 9 5 | 3 4 7
+# 3 7 4 | 6 8 2 | 9 1 5
+# 9 5 1 | 7 4 3 | 6 2 8
+# ------+-------+------
+# 5 1 9 | 3 2 6 | 8 7 4
+# 2 4 8 | 9 5 7 | 1 3 6
+# 7 6 3 | 4 1 8 | 2 5 9
 
 # Creates a new instance of the Sudoku class passing the values array
 sudoku = Sudoku(values)
+sudoku.parse_grid()
+print(sudoku.grid)
