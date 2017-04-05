05990f48b811e96d467281d5c80f3fa032db9750
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-04-05 16:50:48 -0400

    Start a grid parse function
    ...And reformatted the puzzle answer. (It was bothering me. :P)

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from collections import OrderedDict
+
 class Sudoku(object):
     """
     Sudoku(values) -> new instance
@@ -23,7 +25,7 @@ class Sudoku(object):
     rows = [[], [], [], [], [], [], [], [], []]
     columns = [[], [], [], [], [], [], [], [], []]
     squares = [[], [], [], [], [], [], [], [], []]
-    grid = {}
+    grid = OrderedDict()
 
     def __init__(self, values):
         """See class docstring for details."""
@@ -55,6 +57,11 @@ class Sudoku(object):
                 for k in range(0, 3):
                     self.squares[i].append(self.rows[j][k])
 
+    def parse_grid(self):
+        for l in 'ABCDEFGHI':
+            for i in range(1, 10):
+                self.grid[l + str(i)] = ''
+
 # An example sudoku stored as an 81 value array
 values = [0, 0, 0, 2, 6, 0, 7, 0, 1,
           6, 8, 0, 0, 7, 0, 0, 9, 0,
@@ -67,17 +74,17 @@ values = [0, 0, 0, 2, 6, 0, 7, 0, 1,
           7, 0, 3, 0, 1, 8, 0, 0, 0]
 
 # Answer to example sudoku:
-# 4 3 5 | 2 6 9 | 7 8 1
-# 6 8 2 | 5 7 1 | 4 9 3
-# 1 9 7 | 8 3 4 | 5 6 2
-# ------+-------+------
-# 8 2 6 | 1 9 5 | 3 4 7
-# 3 7 4 | 6 8 2 | 9 1 5
-# 9 5 1 | 7 4 3 | 6 2 8
-# ------+-------+------
-# 5 1 9 | 3 2 6 | 8 7 4
-# 2 4 8 | 9 5 7 | 1 3 6
-# 7 6 3 | 4 1 8 | 2 5 9
+# 4  3  5 | 2  6  9 | 7  8  1
+# 6  8  2 | 5  7  1 | 4  9  3
+# 1  9  7 | 8  3  4 | 5  6  2
+# --------+---------+--------
+# 8  2  6 | 1  9  5 | 3  4  7
+# 3  7  4 | 6  8  2 | 9  1  5
+# 9  5  1 | 7  4  3 | 6  2  8
+# --------+---------+--------
+# 5  1  9 | 3  2  6 | 8  7  4
+# 2  4  8 | 9  5  7 | 1  3  6
+# 7  6  3 | 4  1  8 | 2  5  9
 
 # Creates a new instance of the Sudoku class passing the values array
 sudoku = Sudoku(values)
